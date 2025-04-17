7e1acbf4545d1e99bf10cda2942930e0872f6c56
David Leandro Medina Soloza <david.medsol@outlook.es>
2025-04-17 11:16:20 +0100

    chore: replace biome with eslint configuration

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import js from "@eslint/js";
+
+export default [
+  { languageOptions: { globals: globals.browser } },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReactConfig,
+  {
+    files: ["**/*.{ts,tsx}"], // Apply React specific settings only to TS/TSX files
+    settings: {
+      react: {
+        version: "detect", // Automatically detect the React version
+      },
+    },
+    rules: {
+        // Aquí puedes añadir o sobrescribir reglas específicas para React o TypeScript
+        // Ejemplo: "@typescript-eslint/no-explicit-any": "warn"
+    }
+  },
+  {
+    ignores: ["dist", "node_modules", "**/*.config.js", "**/*.config.ts", "**/*.cjs"], // Ignore build outputs, dependencies and config files
+  },
+]; 
