b73baf78767ab88330f461d78da631efd56ef129
David Leandro Medina Soloza <david.medsol@outlook.es>
2025-04-17 11:22:32 +0100

    style: format code

--- eslint.config.js
+++ eslint.config.js
@@ -5,23 +5,29 @@ import js from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
-  { languageOptions: { globals: globals.browser } },
-  js.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReactConfig,
-  {
-    files: ["**/*.{ts,tsx}"], // Apply React specific settings only to TS/TSX files
-    settings: {
-      react: {
-        version: "detect", // Automatically detect the React version
-      },
-    },
-    rules: {
-      "react/react-in-jsx-scope": "off"
-    }
-  },
-  {
-    ignores: ["dist", "node_modules", "**/*.config.js", "**/*.config.ts", "**/*.cjs"], // Ignore build outputs, dependencies and config files
-  },
-  eslintConfigPrettier,
-]; 
+	{ languageOptions: { globals: globals.browser } },
+	js.configs.recommended,
+	...tseslint.configs.recommended,
+	pluginReactConfig,
+	{
+		files: ["**/*.{ts,tsx}"], // Apply React specific settings only to TS/TSX files
+		settings: {
+			react: {
+				version: "detect", // Automatically detect the React version
+			},
+		},
+		rules: {
+			"react/react-in-jsx-scope": "off",
+		},
+	},
+	{
+		ignores: [
+			"dist",
+			"node_modules",
+			"**/*.config.js",
+			"**/*.config.ts",
+			"**/*.cjs",
+		], // Ignore build outputs, dependencies and config files
+	},
+	eslintConfigPrettier,
+];
